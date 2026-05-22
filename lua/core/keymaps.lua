990a9e120fb891aaddccd1351f2d01f1fbb7913e
pcaplat <pcaplat@42angouleme.fr>
2026-05-22 09:07:51 +0200

    add lazyvim and tokyonight colorscheme

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,5 +2,5 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap.set
 
-keymap("n", "-", vim.cmd.Ex)
+keymap("n", "-", vim.cmd.Ex, {desc = "return to file explorator"})
 keymap("n", "<leader>pv", vim.cmd.Ex)
